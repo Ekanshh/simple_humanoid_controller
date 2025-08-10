e55e487d9a98080bab3a0e2406bf3a0cfeb66887
Ekansh Sharma <sharma.ekansh@protonmail.com>
2025-08-10 22:38:03 +0200

    feat: try a new example to move arm to target pose and look at it
    - Move arm to a target pose and back, while camera looks at the hand
    - Add related task and constraint setup in controller

--- src/SimpleHumanoidController.cpp
+++ src/SimpleHumanoidController.cpp
@@ -5,26 +5,85 @@ SimpleHumanoidController::SimpleHumanoidController(mc_rbdyn::RobotModulePtr rm,
                                                    const mc_rtc::Configuration & config)
 : mc_control::MCController(rm, dt)
 {
-  jointIndex = robot().jointIndexByName(jointName);
-
   config_.load(config);
+
+  // Ensure that contacts surfaces don't move once they are set
   solver().addConstraintSet(contactConstraint);
-  solver().addConstraintSet(kinematicsConstraint);
+
+  // Ensure the robot is subject to joint position and velocity limits
+  // solver().addConstraintSet(kinematicsConstraint);
+
+  // Switch to dynamics mode, this will enable us to:
+  //   - Compute external forces and associated torques;
+  //   - Ensure these forces remain in the contact friction cone;
+  //   - Ensure the torques remain within the robot's torque limits;
+  // All constraints enabled by kinematicsConstraint are also enabled by dynamicsConstraint.
+  solver().addConstraintSet(dynamicsConstraint);
+
+  // Add contacts between the robot's feet and the ground:
+  addContact({robot().name(), "ground", "LeftFoot", "AllGround"});
+  addContact({robot().name(), "ground", "RightFoot", "AllGround"});
+
+  // Add self-collision constraint
+  selfCollisionConstraint = std::make_shared<mc_solver::CollisionsConstraint>(robots(), 0, 0, solver().dt());
+  solver().addConstraintSet(*selfCollisionConstraint);
+  selfCollisionConstraint->addCollisions(solver(), robot().module().minimalSelfCollisions());
+
+  /* Tasks */
+  // Task to control the robot posture, tries to bring all joints toward a target posture (usually the robot’s home)
+  comTask = std::make_shared<mc_tasks::CoMTask>(robots(), 0, 10.0, 1000.0);
+  solver().addTask(comTask);
   solver().addTask(postureTask);
-  solver().setContacts({{}});
+  postureTask->stiffness(0.1);
+  // Add hand task
+  handTask = std::make_shared<mc_tasks::SurfaceTransformTask>("LeftGripper", robots(), 0);
+  solver().addTask(handTask);
+  // Look at frame task
+  lookAtFrameTask = std::make_shared<mc_tasks::LookAtFrameTask>(robots(),
+                                                                0, // robot index
+                                                                "dcamera", // body to control
+                                                                Eigen::Vector3d(0, 0, 1), // gaze vector (z axis)
+                                                                0, // target robot index (same robot here)
+                                                                "LeftGripper", // target frame name
+                                                                10.0, // stiffness
+                                                                500.0 // weight
+  );
+  solver().addTask(lookAtFrameTask);
+
+  const auto & pose = handTask->surfacePose();
+  initial_trans = pose.translation();
+  initial_quat = Eigen::Quaterniond(pose.rotation());
+  initial_rot = initial_quat.normalized().toRotationMatrix();
+
+  target_trans = Eigen::Vector3d(0.5, 0.25, 1.1);
+  target_quat = Eigen::Quaterniond(0, 0.7, 0, 0.7).normalized();
+  target_rot = target_quat.toRotationMatrix();
+
+  goToTarget = true;
+
+  tol = 0.01;
+
+  // Print all one-DoF joints (non-mimic) of the robot you control
+  for(const auto & j : robot().mb().joints())
+  {
+    if(j.dof() >= 1 && !j.isMimic())
+    {
+      mc_rtc::log::info("- {}", j.name());
+    }
+  }
 
   mc_rtc::log::success("SimpleHumanoidController init done ");
 }
 
 bool SimpleHumanoidController::run()
 {
-  if(std::abs(postureTask->posture()[jointIndex][0] - robot().mbc().q[jointIndex][0]) < 0.05)
+  if(handTask->eval().norm() < tol)
   {
     switch_target();
   }
+
   return mc_control::MCController::run();
 }
-
 void SimpleHumanoidController::reset(const mc_control::ControllerResetData & reset_data)
 {
   mc_control::MCController::reset(reset_data);
@@ -32,15 +91,15 @@ void SimpleHumanoidController::reset(const mc_control::ControllerResetData & res
 
 void SimpleHumanoidController::switch_target()
 {
-  if(goingLeft)
+  if(goToTarget)
   {
-    postureTask->target({{jointName, robot().qu()[jointIndex]}});
+    handTask->target(sva::PTransformd{target_rot, target_trans});
   }
   else
   {
-    postureTask->target({{jointName, robot().ql()[jointIndex]}});
+    handTask->target(sva::PTransformd{initial_rot, initial_trans});
   }
-  goingLeft = !goingLeft;
+  goToTarget = !goToTarget;
 }
 
 CONTROLLER_CONSTRUCTOR("SimpleHumanoidController", SimpleHumanoidController)
--- src/SimpleHumanoidController.h
+++ src/SimpleHumanoidController.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <mc_control/mc_controller.h>
-
+#include <mc_rbdyn/Robot.h>
+#include <mc_solver/CollisionsConstraint.h>
+#include <mc_tasks/CoMTask.h>
+#include <mc_tasks/LookAtFrameTask.h>
+#include <mc_tasks/SurfaceTransformTask.h>
 #include "api.h"
 
 struct SimpleHumanoidController_DLLAPI SimpleHumanoidController : public mc_control::MCController
@@ -16,7 +20,19 @@ struct SimpleHumanoidController_DLLAPI SimpleHumanoidController : public mc_cont
 
 private:
   mc_rtc::Configuration config_;
-  std::string jointName = "NECK_Y";
-  int jointIndex = 0;
-  bool goingLeft = true;
+  std::shared_ptr<mc_solver::CollisionsConstraint> selfCollisionConstraint;
+  std::shared_ptr<mc_tasks::CoMTask> comTask;
+  std::shared_ptr<mc_tasks::SurfaceTransformTask> handTask;
+  std::shared_ptr<mc_tasks::LookAtFrameTask> lookAtFrameTask;
+  bool goToTarget;
+
+  Eigen::Vector3d target_trans;
+  Eigen::Quaterniond target_quat;
+  Eigen::Matrix3d target_rot;
+
+  Eigen::Vector3d initial_trans;
+  Eigen::Quaterniond initial_quat;
+  Eigen::Matrix3d initial_rot;
+
+  double tol;
 };

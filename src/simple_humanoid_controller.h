f37bdcf640280c2c722c791894748b6fd17fa86a
EKansh Sharma <sharma.ekansh@protonmail.com>
2025-08-14 15:00:03 +0200

    style: make member string constants const and clean up formatting
    - Make frame_id and pose_key member variables const
    - Remove extra blank lines for cleaner code structure

--- src/simple_humanoid_controller.h
+++ src/simple_humanoid_controller.h
@@ -2,23 +2,20 @@
 
 #include <mc_control/fsm/Controller.h>
 #include <mc_control/mc_controller.h>
-
 #include "api.h"
 
 struct SimpleHumanoidController_DLLAPI SimpleHumanoidController : public mc_control::fsm::Controller
 {
   SimpleHumanoidController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config);
-
   bool run() override;
-
   void reset(const mc_control::ControllerResetData & reset_data) override;
 
 private:
-  std::string left_hand_frame_id_ = "LeftGripper";
-  std::string right_hand_frame_id_ = "RightGripper";
-  std::string left_hand_initial_pose_key_ = "LeftHandInitialPose";
-  std::string right_hand_initial_pose_key_ = "RightHandInitialPose";
-
   void read_and_store_left_hand_initial_pose();
   void read_and_store_right_hand_initial_pose();
+
+  const std::string left_hand_frame_id_;
+  const std::string right_hand_frame_id_;
+  const std::string left_hand_initial_pose_key_;
+  const std::string right_hand_initial_pose_key_;
 };
